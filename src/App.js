9d405e60bcf6514266d28bc8432b3e12764d1a34
balibou <benjamin.cherion@gmail.com>
2018-01-21 01:47:50 +0100

    add few login cases

--- src/App.js
+++ src/App.js
@@ -56,18 +56,39 @@ class App extends Component {
         simpleStorageInstance = instance
 
         // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
+        // return simpleStorageInstance.set(5, {from: accounts[0]})
       }).then((result) => {
+        this.setState({ firstAccount: accounts[0] })
         // Get the value from the contract to prove it worked.
         return simpleStorageInstance.get.call(accounts[0])
       }).then((result) => {
         // Update state with the result.
         return this.setState({ storageValue: result.c[0] })
+      }).catch((err) => {
+        return this.setState({ error: 'wrong network' })
       })
     })
   }
 
   render() {
+    if (this.state.error){
+      return (
+        <div className="App">
+        <nav className="navbar pure-menu pure-menu-horizontal">
+            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+        </nav>
+
+        <main className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              <p>Oops, you’re on the wrong network</p>
+              <p>Simply open MetaMask and switch over to the Main Ethereum Network.</p>
+            </div>
+          </div>
+        </main>
+      </div>
+      )
+    }
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
@@ -83,6 +104,19 @@ class App extends Component {
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
               <p>The stored value is: {this.state.storageValue}</p>
+
+              {
+                this.state.firstAccount === '0xb185e019f3164386c9d8c94bcb734e70010b7e2e'
+                ? <div>
+                  <p>You have an account on our platform !</p>
+                </div>
+                : <div>
+                  {/*Good network but no account*/}
+                  {/*Associate a mail and a nickname to that account*/}
+                  <p>Wallet address:</p>
+                  <p>{this.state.firstAccount}</p>
+                </div>
+              }
             </div>
           </div>
         </main>
